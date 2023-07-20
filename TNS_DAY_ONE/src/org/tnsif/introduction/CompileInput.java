b986ee954a382bb927b0a13b0a6f6e1fc4aac920
Shree <Shree@DESKTOP-T5LTQNS>
2023-07-20 14:07:22 +0530

    first commit

---
+++ TNS_DAY_ONE/src/org/tnsif/introduction/CompileInput.java
@@ -0,0 +1,14 @@
+//Program to demonstrate on Compile-Time-Input
+package org.tnsif.introduction;
+
+public class CompileInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		short num=34;
+		System.out.println("The Num is:"+num);
+		
+	}
+
+}
---
+++ TNS_DAY_ONE/src/org/tnsif/introduction/UserInputDemo.java
@@ -0,0 +1,36 @@
+//Program to demonstrate on User-Input
+package org.tnsif.introduction;
+import java.util.*;
+
+public class UserInputDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner s=new Scanner(System.in);
+		
+		//String
+		System.out.println("Enter a String");
+		//next() 
+		String str=s.next();
+		System.out.println("String is "+str);
+		
+		System.out.println("Enter a String");
+		//nextLine()
+		String str1=s.nextLine();
+		System.out.println("String is "+str1);
+		
+		//int
+		System.out.println("Enter Integer");
+		int num=s.nextInt();
+		System.out.println("Integer is"+num);
+		
+		//char
+		System.out.println("Enter a String");
+		char c=s.next().charAt(0);
+		System.out.println("Character at 0 index:"+c);
+		
+		
+	}
+
+}
